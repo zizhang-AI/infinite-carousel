3845d938df6b32f30fde89e65d0a09b57aeb7297
Zizhang <zizhang.ai@ihealthlabs.com>
2018-03-03 21:40:30 +0800

    using mobile example

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,48 +1,85 @@
 import React, { PureComponent } from "react";
 
-const itemStyle = {
-  width: 200,
-  height: 100,
-  backgroundColor: "red",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  flexShrink: 0
-};
-const styles = {
-  container: {
-    transition: "all 2s ease",
-    display: "flex",
-    width: 200,
-    overflow: "hidden",
-    margin: "auto",
-    padding: 0
-  },
-  item1: {
-    ...itemStyle,
-    backgroundColor: "red",
-    transform: "translate(-20px)"
-  },
-  item2: {
-    ...itemStyle,
-    backgroundColor: "green",
-    transform: "translate(-20px)"
-  },
-  item3: {
-    ...itemStyle,
-    backgroundColor: "blue"
-  }
-};
 export default class Carousel extends PureComponent {
-  startPos = 0;
-  endPos = 0;
+  startX = 0;
+  endX = 0;
+  currIdx = 0;
+  cssTransform(ele, attr, val) {
+    if (!ele.transform) {
+      ele.transform = {};
+    }
+    //当传入值时对属性进行设置。
+    if (arguments.length > 2) {
+      ele.transform[attr] = val;
+      var sval = "";
+      for (var s in ele.transform) {
+        if (s === "translateX") {
+          sval += s + "(" + ele.transform[s] + "px)";
+        }
+        //如果实在不理解，可以console.log(sval)可以看到到最后一张时会有一个先跳到第二张再快速到第三张的过程
+        ele.style.WebkitTransform = ele.style.transform = sval;
+      }
+    } else {
+      val = ele.transform[attr];
+      if (typeof val === "undefined") {
+        if (attr === "translateX") {
+          val = 0;
+        }
+      }
+      return val;
+    }
+  }
+  touchStart(e) {
+    e.stopPropagation();
+    this.LunBoEle.style.transition = "none";
+    let moveX = this.cssTransform.bind(this)(this.LunBoEle, "translateX");
+    this.now = Math.round(-moveX / this.sliderWidth);
+    if (this.now === 0) {
+      this.now = this.props.len / 2;
+    } else if (this.now === this.props.len - 1) {
+      this.now = this.props.len / 2 - 1;
+    }
+    this.cssTransform(
+      this.LunBoEle,
+      "translateX",
+      -this.now * this.sliderWidth
+    );
+    this.startPoint = e.changedTouches[0].pageX;
+    this.startEle = this.cssTransform.bind(this)(this.LunBoEle, "translateX");
+  }
+  //移动
+  touchMove(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    let endPoint = e.changedTouches[0].pageX;
+    let disX = endPoint - this.startPoint;
+    this.cssTransform.bind(this)(
+      this.LunBoEle,
+      "translateX",
+      disX + this.startEle
+    );
+  }
+  touchEnd(e) {
+    e.stopPropagation();
+    let moveX = this.cssTransform.bind(this)(this.LunBoEle, "translateX");
+    //这边我是对移动做了判断
+    if (Math.abs(moveX) > Math.abs(this.now * this.sliderWidth)) {
+      this.now = Math.ceil(-moveX / this.sliderWidth);
+    } else {
+      this.now = Math.floor(-moveX / this.sliderWidth);
+    }
+    this.tab.bind(this)();
+    this.auto.bind(this)();
+  }
   render() {
     return (
-      <ul style={styles.container}>
-        <li style={styles.item1}>1</li>
-        <li style={styles.item2}>2</li>
-        <li style={styles.item3}>3</li>
-      </ul>
+      <div className="slider">
+        <div className="content">
+          <div className="page" />
+          <div className="page" />
+          <div className="page" />
+        </div>
+      </div>
     );
   }
 }
